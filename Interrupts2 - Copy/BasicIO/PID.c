98c8dd8df24d6a972e6ea4f77aa14f538b97043e
Peter Zajac <peter.zaja@gmail.com>
2022-01-18 10:54:56 +0100

    Add files via upload

--- Interrupts2 - Copy/BasicIO/PID.c
+++ Interrupts2 - Copy/BasicIO/PID.c
@@ -18,7 +18,7 @@
 
 
 
-float PID_RPM(float RPM_current, float RPM_setpoint, float RPM_time,float Kp, float Ki)
+float PID_RPM(float RPM_current, float RPM_setpoint, float RPM_time)
 {
 	static float RPM_lastinput=0;
 	static double I_error=0;
@@ -27,20 +27,8 @@ float PID_RPM(float RPM_current, float RPM_setpoint, float RPM_time,float Kp, fl
 	
 	I_error+=P_error*RPM_time;
 	
-	float D_error=0;//(RPM_current-RPM_lastinput)/RPM_time;
+	float D_error=(RPM_current-RPM_lastinput)/RPM_time;
 	RPM_lastinput=RPM_current;
-	return (PID_KP*P_error+PID_KI*I_error+PID_KD*D_error);
+	return (PID_KP*P_error+PID_KI*I_error-PID_KD*D_error);
 }
 
-float PID_CURRENT(float i_current,float i_desired, float i_time) //i stands for electric current
-{
-	double i_err=i_desired-i_current;
-	static double I2_error=0;
-	//static float i_lastinput=0;
-	
-	I2_error+=i_err*i_time;
-	
-	//i_lastinput=i_current;
-	return (PID_KC*i_err+PID_KI2*I2_error);
-	
-}
--- Interrupts2 - Copy/BasicIO/PID.h
+++ Interrupts2 - Copy/BasicIO/PID.h
@@ -11,17 +11,17 @@
 
 #define PID_KC 0.00073/0.000208 //Kp term for the current ctrl La/Tdesired
 #define PID_KI2 0.7/0.000208 //calculate Time desired Ra/Tdesired
-#define PID_KP 1 //should be Kn
-#define PID_KI 0.1 //should be Kn/Tn
-#define PID_KD 10
+#define PID_KP 0.2 //should be Kn
+#define PID_KI 0.01 //should be Kn/Tn 
+#define PID_KD 0.1
 #define I_MAX 8.7
 #define I_MIN 0
 #define RPM_MAX 3430
 #define RPM_MIN 0
-#define OCR1A_MAX 70
+#define OCR1A_MAX 70.0
 #define OCR1A_MIN 0
 
-float PID_RPM(float RPM_current, float RPM_setpoint, float RPM_time,float,float);
-float PID_CURRENT(float i_current,float i_desired, float time);
+float PID_RPM(float RPM_current, float RPM_setpoint, float RPM_time);
+
 
 #endif /* PID_H_ */
